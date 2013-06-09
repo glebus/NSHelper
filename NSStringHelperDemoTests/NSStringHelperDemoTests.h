ec283195f39ff0a5b573ee19975052d50d1e8018
glebus <admin@iMac-admin.local>
2013-06-09 14:51:22 +0300

    Initial Commit

---
+++ NSStringHelperDemoTests/NSStringHelperDemoTests.h
@@ -0,0 +1,13 @@
+//
+//  NSStringHelperDemoTests.h
+//  NSStringHelperDemoTests
+//
+//  Created by glebus on 09.06.13.
+//  Copyright (c) 2013 glebus. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NSStringHelperDemoTests : SenTestCase
+
+@end
